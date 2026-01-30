eb6bc5f14f6d7da8ce7d0e9e5095f21dbef4ae4f
theWhite-Knight <al547645@georgetownisd.org>
2026-01-30 09:48:23 -0600

    mark 2

--- thirtyThree.py
+++ thirtyThree.py
@@ -1,7 +1,7 @@
 # ALex's take on "Ingredients and Money"
-# Money
-import sys
 #-------
+import sys
+# Money
 money = 200
 #-------
 # Ingredients
@@ -24,53 +24,61 @@ ingredients = {
 }
 #-------
 def shop():
+    global money
     print("You have entered the shop!") # Takes count of what the user wants >>
     print("------------------------------------------------------")
     when = input("What would you like to purchase? \
-    (1) Lemons [1.99 per], (2) Sugar [0.60 per lb], (3) Cups [0.50 per], (4) Ice [0.99 per lb]- ")
+    (1) Lemons [1.99 per], (2) Sugar [0.60 per lb], (3) Cups [0.50 per], (4) Ice [0.99 per lb]:\n" "Enter EXIT to exit.\n")
     # Math on the cost*Usercount for each ingredient >>
     if when == "1":
         print("You have decided to pick Lemons!")
-        amount_1 = int(input("How much would you like to buy? - "))
+        amount_1 = int(input("How much would you like to buy?:\n"))
         if money*lemonPrice >= money:
-            print("You dont have enough money")
+            print("You don't have enough money")
             shop()
-        if money*lemonPrice <= money:
-            money-amount_1*lemonPrice
+        elif money*lemonPrice <= money:
+            money = money-amount_1*lemonPrice
+            print(money)
             shop()     
     if when == "2":
         print("You have decided to pick Sugar!")
-        amount_2 = int(input("How much would you like to buy? - "))
+        amount_2 = int(input("How much would you like to buy?:\n"))
         if money*sugarPrice >= money:
             print("You don't have enough money")
             shop()
-        if money*sugarPrice <= money:
-            money-amount_1*sugarPrice
+        elif money*sugarPrice <= money:
+            money = money-amount_2*sugarPrice
+            print(money)
             shop()
     if when == "3":
         print("You have decided to pick Cups!")
-        amount_3 = int(input("How much would you like to buy? - "))
+        amount_3 = int(input("How much would you like to buy?:\n"))
         if money*cupPrice >= money:
             print("You don't have enough money")
+            print(money)
             shop()
-        if money*cupPrice <= money:
-            money-amount_1*cupPrice
+        elif money*cupPrice <= money:
+            money = money-amount_3*cupPrice
+            print(money)
             shop()
     if when == "4":
         print("You have decided to pick Ice!")
-        amount_4 = int(input("How much would you like to buy? - "))
-         if money*icePrice >= money:
+        amount_4 = int(input("How much would you like to buy?:\n"))
+        if money*icePrice >= money:
             print("You don't have enough money")
+            print(money)
             shop()
-        if money*icePrice <= money:
-            money-amount_1*icePrice
+        elif money*icePrice <= money:
+            money = money-amount_4*icePrice
+            print(money)
             shop()
+    if when == "EXIT":
+        print(f"Your balance is: {money}")
+        sys.exit()
     else:
-        print("Please try again...") # For if the user is low on money.
-    print(f"Your balance is: {money}")
         shop()
 #-------
-if Money <= 0:
+if money <= 0:
     print("You lost!")
     print("------------------")
     print("GAME OVER!")
