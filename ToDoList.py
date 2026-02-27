20a7b8fa1bd244a2a3298a1ab328af71e48f4dd2
ImJetO <jo558915@georgetownisd.org>
2026-02-26 20:16:15 -0600

    UPDATED TODO LIST
    OH YEAH

--- ToDoList.py
+++ ToDoList.py
@@ -1,15 +1,49 @@
 # TODO
 
+print("TODO LIST")
+print("Jet's Version")
+
+
 # [X] = Done / Check
 # [X / ] = Half done / Almost
 # [ ] = Not Done yet
 # { } Option (Not required)
 
+# NOTE: The following Steps Are NOT in order, and try not to go down the list in order. Also, please try not to delete Steps if you think
+#       that you don't like it or something. Either tell someone in person in advanced or just Comment Next to it please! Adding Steps is Great though.😊
+
+# NOTE: Save the 'Option Steps' for last unless it litterly takes like 10 minutes, This is only to optimize our time wisley and not
+#       spend half the project time wasting our time on things like weather.
+
+# ------------------------------------------------------------------------------------------------------------------------
+
+# Main Gameplay
 
 # [X] 1. Introduction, Front Page, First Impression
 # [X] 2. First Disision of what action you want to do
 # [X] 3. Create a Fully working Shop with minimal to no Errors
 # [X / ] 4. Create a Almost to Fully working Recipe System
-# { } 5. Add Weather System that effects customer prefrence
-# { } 6. Add Upgrades / Diffulculties that effect Money, Earnings, and other neutral variables
-# {X / } 7. Add Cheat codes that only specified people know
+# {X / } 5. Add Cheat codes that only specified people know
+# [X] 6. Create code that changes your price for customers to buy
+# [X / ] 7. Create code for 'Customer Prefrence' for selling
+# [X / ] 8. Have a option for starting the day and sell your product to customers
+
+# ------------------------------------------------------------------------------------------------------------------------
+
+# Options
+
+# { } 1. Add Weather System that effects customer prefrence
+# { } 2. Add Upgrades / Diffulculties that effect Money, Earnings, and other neutral variables
+# { } 3. Add a Story Line or some fun Lore to get peoples intrest
+# { } 4. At the end of the game add a working roll that has Credits on what people worked on    -  (Jet was planning to do this already but the code got
+#  deleated somehow, so it'll have to get remade better than before. Besides, it will be so cool!) 
+
+# ------------------------------------------------------------------------------------------------------------------------
+
+# Cheat Code Ideas - NOTE: Do NOT Delete any Ideas, remember they're all NOT Official Yet
+
+# NOTE: I, Jet, was thinking the codes will work only ONCE throughout the gameplay. It'll reset if the program resets!
+
+# EndOfLine    - Gives a lot of Money         - (Quote from Tron, NOTE: Its a really good movie series except for the first movie)
+# ADMIN_ABUSE    - Gives 100 ingredients of Lemons, Sugar, Ice, and Cups + Money
+# AreWeSerious?!    - Times the Player income by 1.2x
