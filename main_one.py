a2cf3403b8d9e975d47a210fc1a780c03f03d220
ImJetO <jo558915@georgetownisd.org>
2026-03-02 09:47:43 -0600

    fixed the end
    ++

--- main_one.py
+++ main_one.py
@@ -26,7 +26,11 @@ ingredients = {
 
 
 
-
+if money <= 0:
+    print("You lost!")
+    print("------------------")
+    print("GAME OVER!")
+    sys.exit()
 
 #-------
 print("------------------------------")
@@ -52,7 +56,7 @@ if p.lower() in ("y", "yes", "ok", "okay", "sure"):
         global ingredients
         while day <= 7:
             LemonSet = float(input("What would you like to set your Lemonade price to?\n   (Note: You can change this later, and add decimals to the hundreths place)\n"))
-            
+
             if LemonSet.isdigit():
                 print(f"You have {money} Money. \nYour Inventory contains of {ingredients}. \nYou are on Day {day}. \nYour Recipe consists of {Usage}.\n")
                 print(f"The price of your Lemonade is {LemonSet}")
@@ -211,8 +215,4 @@ else:
     print("Invalid input. Exiting...")
     sys.exit()
 
-if money <= 0:
-    print("You lost!")
-    print("------------------")
-    print("GAME OVER!")
-    sys.exit()
+
